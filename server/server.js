bafab2f01b0e086e91631aab236cc4b36a4ebd55
SIHAAB <mirzashihab2@gmail.com>
2023-04-23 14:29:26 +0200

    need to start asap

--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require('express');
 
+require('dotenv').config();
+
 const app = express();
+const PORT = process.env.PORT;
 
 app.get('/test', (req, res) => {
     return res.send("ok");
 })
 
-app.listen(3000, () => {
+app.listen(PORT || 4455, () => {
     console.log('running');
 })
 
