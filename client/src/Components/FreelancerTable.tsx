87265bdd18d3bf7998c35b5eada67570fc94ddf0
SIHAAB <mirzashihab2@gmail.com>
2023-04-25 15:47:10 +0200

    admin thing done

--- client/src/Components/FreelancerTable.tsx
+++ client/src/Components/FreelancerTable.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import Swal from "sweetalert2";
 
 interface Freelancer {
 	username: string;
@@ -12,6 +13,23 @@ interface Props {
 }
 
 const FreelancerTable = ({ freelancers }: Props) => {
+
+	
+	const delete_freelancer = (username: string) => {
+		Swal.fire({
+			title: "Are you sure you want to remove this freelancer? this will un-link them from any projects they're attached to.",
+			icon: "warning",
+			showCancelButton: true,
+			confirmButtonColor: "#3085d6",
+			cancelButtonColor: "#d33",
+			confirmButtonText: "Yes",
+		}).then((result) => {
+			if (result.isConfirmed) {
+				alert('Removed ' + username)
+			}
+		});
+	}
+
 	return (
 		<table className="table table-hover table-dark p-5">
 			<thead className="thead-dark">
@@ -32,7 +50,9 @@ const FreelancerTable = ({ freelancers }: Props) => {
 						<td>{freelancer.age}</td>
                         <td>{freelancer.email}</td>
                         <td><button className="btn btn-primary">Edit</button></td>
-                        <td><button className="btn btn-danger">Remove</button></td>
+						<td><button className="btn btn-danger" onClick={() => {
+							delete_freelancer(freelancer.username);
+						}}>Remove</button></td>
                         <td><button className="btn btn-warning">Message</button></td>
 					</tr>
 				))}
---
+++ client/src/Pages/Admin/EditFreelancer.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Navbar from '../../Components/Navbar'
+
+export default function EditFreelancer() {
+  return (
+      <div className="d-flex">
+        <Navbar priv='admin'/>
+
+          <div className="container">
+              Edit freelancer
+        </div>
+      </div>
+  )
+}
